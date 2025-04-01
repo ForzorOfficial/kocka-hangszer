1f9f494d1b5953918dfae3e89d60add7839c8d17
kabaiakos <info@forzor.hu>
2025-04-01 12:30:32 +0200

    Test

---
+++ src/index.ts
@@ -0,0 +1,363 @@
+
+import './style.css'
+
+import $ from 'jquery';
+import { Subscription, interval } from 'rxjs';
+import { TwistyPlayer } from 'cubing/twisty';
+import { experimentalSolve3x3x3IgnoringCenters } from 'cubing/search';
+
+import * as THREE from 'three';
+
+import {
+  now,
+  connectGanCube,
+  GanCubeConnection,
+  GanCubeEvent,
+  GanCubeMove,
+  MacAddressProvider,
+  makeTimeFromTimestamp,
+  cubeTimestampCalcSkew,
+  cubeTimestampLinearFit
+} from 'gan-web-bluetooth';
+
+import { faceletsToPattern, patternToFacelets } from './utils';
+
+const SOLVED_STATE = "UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB";
+
+var twistyPlayer = new TwistyPlayer({
+  puzzle: '3x3x3',
+  visualization: 'PG3D',
+  alg: '',
+  experimentalSetupAnchor: 'start',
+  background: 'none',
+  controlPanel: 'none',
+  hintFacelets: 'none',
+  experimentalDragInput: 'none',
+  cameraLatitude: 0,
+  cameraLongitude: 0,
+  cameraLatitudeLimit: 0,
+  tempoScale: 5
+});
+
+$('#cube').append(twistyPlayer);
+
+var conn: GanCubeConnection | null;
+var lastMoves: GanCubeMove[] = [];
+var solutionMoves: GanCubeMove[] = [];
+
+var twistyScene: THREE.Scene;
+var twistyVantage: any;
+
+let lastMove: string | null = null;
+let moveTimeout: number | null = null;
+
+const HOME_ORIENTATION = new THREE.Quaternion().setFromEuler(new THREE.Euler(15 * Math.PI / 180, -20 * Math.PI / 180, 0));
+var cubeQuaternion: THREE.Quaternion = new THREE.Quaternion().setFromEuler(new THREE.Euler(30 * Math.PI / 180, -30 * Math.PI / 180, 0));
+
+async function amimateCubeOrientation() {
+  if (!twistyScene || !twistyVantage) {
+    var vantageList = await twistyPlayer.experimentalCurrentVantages();
+    twistyVantage = [...vantageList][0];
+    twistyScene = await twistyVantage.scene.scene();
+  }
+  twistyScene.quaternion.slerp(cubeQuaternion, 0.25);
+  twistyVantage.render();
+  requestAnimationFrame(amimateCubeOrientation);
+}
+requestAnimationFrame(amimateCubeOrientation);
+
+var basis: THREE.Quaternion | null;
+
+async function handleGyroEvent(event: GanCubeEvent) {
+  if (event.type == "GYRO") {
+    let { x: qx, y: qy, z: qz, w: qw } = event.quaternion;
+    let quat = new THREE.Quaternion(qx, qz, -qy, qw).normalize();
+    if (!basis) {
+      basis = quat.clone().conjugate();
+    }
+    cubeQuaternion.copy(quat.premultiply(basis).premultiply(HOME_ORIENTATION));
+    $('#quaternion').val(`x: ${qx.toFixed(3)}, y: ${qy.toFixed(3)}, z: ${qz.toFixed(3)}, w: ${qw.toFixed(3)}`);
+    if (event.velocity) {
+      let { x: vx, y: vy, z: vz } = event.velocity;
+      $('#velocity').val(`x: ${vx}, y: ${vy}, z: ${vz}`);
+    }
+  }
+}
+
+function playMoveSound(move: string ) {
+  let soundFile = '';
+
+  switch (move) {
+      case 'R':
+          soundFile = '/sounds/c3.mp3';
+          break;
+      case 'L':
+          soundFile = '/sounds/d3.mp3';
+          break;
+      case 'U':
+          soundFile = '/sounds/e3.mp3';
+          break;
+      case 'D':
+          soundFile = '/sounds/f3.mp3';
+          break;
+      case 'F':
+          soundFile = '/sounds/g3.mp3';
+          break;
+      case 'B':
+          soundFile = '/sounds/a4.mp3';
+          break;
+      case "M'":
+          soundFile = '/sounds/b3.mp3';
+          break;
+      case "R'":
+          soundFile = '/sounds/c4.mp3';
+          break;
+      case "L'":
+          soundFile = '/sounds/d4.mp3';
+          break;
+      case "U'":
+          soundFile = '/sounds/e4.mp3';
+          break;
+      case "D'":
+          soundFile = '/sounds/f4.mp3';
+          break;
+      case "F'":
+          soundFile = '/sounds/g4.mp3';
+          break;
+      case "B'":
+          soundFile = '/sounds/a5.mp3';
+          break;
+      case "M":
+          soundFile = '/sounds/b4.mp3';
+          break;
+      default:
+          console.warn('No sound assigned for move:', move);
+          return;
+  }
+  console.log("Sound played: " + soundFile);
+  const sound = new Audio(soundFile);
+  sound.play().catch(error => console.error('Error playing audio:', error));
+}
+
+async function handleMoveEvent(event: GanCubeEvent) {
+  if (event.type == "MOVE") {
+    if (timerState == "READY") {
+      setTimerState("RUNNING");
+    }
+    console.log(event.move);
+
+    // Clear previous timeout if another move comes quickly
+    if (moveTimeout) {
+      clearTimeout(moveTimeout);
+      moveTimeout = null;
+  }
+  
+ if (lastMove === "L" && event.move === "R'") {
+        console.log("Detected L -> R' sequence, playing M'");
+        playMoveSound("M'");
+        lastMove = null; // Reset
+    }
+    else if (lastMove === "R" && event.move === "L'") {
+      console.log("Detected R -> L' sequence, playing M");
+      playMoveSound("M");
+      lastMove = null; // Reset
+    }
+    else {
+        lastMove = event.move;
+        moveTimeout = setTimeout(() => {
+            playMoveSound(event.move);
+            lastMove = null;
+        }, 100); // Short delay to check for next move
+    }
+  
+
+    twistyPlayer.experimentalAddMove(event.move, { cancel: false });
+    lastMoves.push(event);
+    if (timerState == "RUNNING") {
+      solutionMoves.push(event);
+    }
+    if (lastMoves.length > 256) {
+      lastMoves = lastMoves.slice(-256);
+    }
+    if (lastMoves.length > 10) {
+      var skew = cubeTimestampCalcSkew(lastMoves);
+      $('#skew').val(skew + '%');
+    }
+  }
+}
+
+function checkIfM(event: GanCubeEvent, move: string) : string
+{
+
+  console.log(lastMoves);
+
+  if (lastMoves[lastMoves.length - 2] != null){
+    console.log("1");
+    if (lastMoves[lastMoves.length - 2].localTimestamp! - event.timestamp < 15) {
+      console.log("2");
+      console.log(lastMoves[lastMoves.length - 1].move);
+      console.log(move);
+      if (lastMoves[lastMoves.length - 1].move == "L" && move == "R'") { // Check if M'
+        console.log("3")
+
+        return ("M'");
+      }
+    }
+  }
+  
+
+  return(move);
+}
+
+var cubeStateInitialized = false;
+
+async function handleFaceletsEvent(event: GanCubeEvent) {
+  if (event.type == "FACELETS" && !cubeStateInitialized) {
+    if (event.facelets != SOLVED_STATE) {
+      var kpattern = faceletsToPattern(event.facelets);
+      var solution = await experimentalSolve3x3x3IgnoringCenters(kpattern);
+      var scramble = solution.invert();
+      twistyPlayer.alg = scramble;
+    } else {
+      twistyPlayer.alg = '';
+    }
+    cubeStateInitialized = true;
+    console.log("Initial cube state is applied successfully", event.facelets);
+  }
+}
+
+function handleCubeEvent(event: GanCubeEvent) {
+  if (event.type != "GYRO")
+    console.log("");
+    console.log("GanCubeEvent", event);
+
+  if (event.type == "GYRO") {
+    handleGyroEvent(event);
+  } else if (event.type == "MOVE") {
+    handleMoveEvent(event);
+  } else if (event.type == "FACELETS") {
+    handleFaceletsEvent(event);
+  } else if (event.type == "HARDWARE") {
+    $('#hardwareName').val(event.hardwareName || '- n/a -');
+    $('#hardwareVersion').val(event.hardwareVersion || '- n/a -');
+    $('#softwareVersion').val(event.softwareVersion || '- n/a -');
+    $('#productDate').val(event.productDate || '- n/a -');
+    $('#gyroSupported').val(event.gyroSupported ? "YES" : "NO");
+  } else if (event.type == "BATTERY") {
+    $('#batteryLevel').val(event.batteryLevel + '%');
+  } else if (event.type == "DISCONNECT") {
+    twistyPlayer.alg = '';
+    $('.info input').val('- n/a -');
+    $('#connect').html('Connect');
+  }
+}
+
+const customMacAddressProvider: MacAddressProvider = async (device, isFallbackCall): Promise<string | null> => {
+  if (isFallbackCall) {
+    return prompt('Unable do determine cube MAC address!\nPlease enter MAC address manually:');
+  } else {
+    return typeof device.watchAdvertisements == 'function' ? null :
+      prompt('Seems like your browser does not support Web Bluetooth watchAdvertisements() API. Enable following flag in Chrome:\n\nchrome://flags/#enable-experimental-web-platform-features\n\nor enter cube MAC address manually:');
+  }
+};
+
+$('#reset-state').on('click', async () => {
+  await conn?.sendCubeCommand({ type: "REQUEST_RESET" });
+  twistyPlayer.alg = '';
+});
+
+$('#reset-gyro').on('click', async () => {
+  basis = null;
+});
+
+$('#connect').on('click', async () => {
+  if (conn) {
+    conn.disconnect();
+    conn = null;
+  } else {
+    conn = await connectGanCube(customMacAddressProvider);
+    conn.events$.subscribe(handleCubeEvent);
+    await conn.sendCubeCommand({ type: "REQUEST_HARDWARE" });
+    await conn.sendCubeCommand({ type: "REQUEST_FACELETS" });
+    await conn.sendCubeCommand({ type: "REQUEST_BATTERY" });
+    $('#deviceName').val(conn.deviceName);
+    $('#deviceMAC').val(conn.deviceMAC);
+    $('#connect').html('Disconnect');
+  }
+});
+
+var timerState: "IDLE" | "READY" | "RUNNING" | "STOPPED" = "IDLE";
+
+function setTimerState(state: typeof timerState) {
+  timerState = state;
+  switch (state) {
+    case "IDLE":
+      stopLocalTimer();
+      $('#timer').hide();
+      break;
+    case 'READY':
+      setTimerValue(0);
+      $('#timer').show();
+      $('#timer').css('color', '#0f0');
+      break;
+    case 'RUNNING':
+      solutionMoves = [];
+      startLocalTimer();
+      $('#timer').css('color', '#999');
+      break;
+    case 'STOPPED':
+      stopLocalTimer();
+      $('#timer').css('color', '#fff');
+      var fittedMoves = cubeTimestampLinearFit(solutionMoves);
+      var lastMove = fittedMoves.slice(-1).pop();
+      setTimerValue(lastMove ? lastMove.cubeTimestamp! : 0);
+      break;
+  }
+}
+
+twistyPlayer.experimentalModel.currentPattern.addFreshListener(async (kpattern) => {
+  var facelets = patternToFacelets(kpattern);
+  if (facelets == SOLVED_STATE) {
+    if (timerState == "RUNNING") {
+      setTimerState("STOPPED");
+    }
+    twistyPlayer.alg = '';
+  }
+});
+
+function setTimerValue(timestamp: number) {
+  let t = makeTimeFromTimestamp(timestamp);
+  $('#timer').html(`${t.minutes}:${t.seconds.toString(10).padStart(2, '0')}.${t.milliseconds.toString(10).padStart(3, '0')}`);
+}
+
+var localTimer: Subscription | null = null;
+function startLocalTimer() {
+  var startTime = now();
+  localTimer = interval(30).subscribe(() => {
+    setTimerValue(now() - startTime);
+  });
+}
+
+function stopLocalTimer() {
+  localTimer?.unsubscribe();
+  localTimer = null;
+}
+
+function activateTimer() {
+  if (timerState == "IDLE" && conn) {
+    setTimerState("READY");
+  } else {
+    setTimerState("IDLE");
+  }
+}
+
+$(document).on('keydown', (event) => {
+  if (event.which == 32) {
+    event.preventDefault();
+    activateTimer();
+  }
+});
+
+$("#cube").on('touchstart', () => {
+  activateTimer();
+});
